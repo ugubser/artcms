a4aae0fa226ba6d37c6f7cf30d4041a8593c3322
ugubser <nospam@gubser.ch>
2025-07-15 14:59:48 +0200

    removed the design tab in the menu

--- src/app/components/shared/page-header.component.ts
+++ src/app/components/shared/page-header.component.ts
@@ -21,8 +21,9 @@ import { SettingsService } from '../../services/settings.service';
           <li><a routerLink="/about" routerLinkActive="active">ABOUT</a></li>
           <li class="nav-divider">|</li>
           <li><a routerLink="/contact" routerLinkActive="active">CONTACT</a></li>
-          <li class="nav-divider">|</li>
+          <!-- <li class="nav-divider">|</li>
           <li><a routerLink="/design" routerLinkActive="active">DESIGN</a></li>
+	 -->
         </ul>
       </nav>
     </header>
